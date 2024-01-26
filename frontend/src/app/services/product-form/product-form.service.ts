d1087dd6a57d8f3e9159df4d411d16da8e35c0b9
Jonas Dias <zdias1413@gmail.com>
2024-01-26 00:28:19 -0300

    feat: add product form service

---
+++ frontend/src/app/services/product-form/product-form.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../product/product.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductFormService {
+
+  isProductFormOpen: boolean = false;
+  productForm!: FormGroup;
+  private router: Router = new Router();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService
+  ) { 
+    this.createProductForm();
+  }
+
+  openForm() {
+    this.isProductFormOpen = true;
+  }
+
+  closeForm() {
+    this.isProductFormOpen = false;
+  }
+
+  createProductForm(): void {
+    this.productForm = this.formBuilder.group({
+      id: [''],
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      amount: ['', Validators.required],
+      imageUrl: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    if (this.productForm.valid) {
+      this.productService.save(this.productForm.value);
+      this.productForm.reset();
+      this.closeForm();
+      
+      setTimeout(() => this.refresh(), 1000);
+    }
+  }
+  
+  private refresh() {
+    this.router.navigateByUrl('/', {skipLocationChange:true}).then(()=> {
+      this.router.navigate([`/products`]);
+    });
+  }
+
+}
